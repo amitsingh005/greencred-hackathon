07c72eddcab1df4cc6514fb930cc8f3d64f04289
Amit Singh <thamitbagel@gmail.com>
2026-03-25 21:43:49 +0530

    step5: auto create user profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -19,3 +23,15 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.full_name or self.user.username
+    
+
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(
+            user=instance,
+            full_name=instance.username,
+            oauth_uid="default"
+        )
